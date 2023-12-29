5fdbec72a6d3ea6a3edaea17c191bc1d7181b469
david baladi <davidbaladi@gmail.com>
2023-12-28 21:53:10 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -27,6 +33,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
